17fcfd07b2e81271f0caeb6faf0e8d2e1c65dd80
anders <anders@nomerstad.com>
2021-09-16 21:51:26 +0200

    Visuelt oppsett

--- src/components/SamiskSamtaleApp.tsx
+++ src/components/SamiskSamtaleApp.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
+import { BodyLong, GuidePanel, Heading } from '@navikt/ds-react';
+import { SamiskSamtaleOrderForm } from './form/samisk-samtale-order-form/SamiskSamtaleOrderForm';
 import style from './SamiskSamtaleApp.module.css';
-import { Alert, BodyLong, GuidePanel, Heading } from '@navikt/ds-react';
 
 export const SamiskSamtaleApp = () => {
     return (
         <div className={style.appContainer}>
-            <Alert variant={'info'} size={'medium'} style={{ marginBottom: '2rem' }}>
-                {
-                    'Hei! Denne tjenesten er under utvikling og er ikke helt klar for lansering ennå.'
-                }
-                {
-                    ' Du er velkommen til å prøve ut tjenesten, men vær oppmerksom på at noe funksjonalitet kan være uferdig.'
-                }
-            </Alert>
             <Heading size={'2xlarge'} className={style.title}>
                 {'Jearaldat bagadallama oažžut sámegillii telefovnnas'}
             </Heading>
             <GuidePanel poster={true} className={style.ingressPanel}>
                 <BodyLong className={style.ingress}>
-                    {'Diŋgo dás davvisámegilli bálvalusa mas vástiduvvo dutnje sámegillii buot NAV – bálvalusain ja oajuin. Mii veahkehit gávdnat mo du áššiin manná, ja veahkehit du dovdat rivttiid ja geatnegasvuođaid mat leat álbmotoadjolága njuolggadusain. Don gávnnat dieđuid iežat áššis neahttabálvalusas nav.no Ditt NAV. Don sáhtát iskat mii dutnje lea máksojuvvon dás:'}
-                    <br/><br/>
-                    {'Don sáhtat ain riŋget NAV-bálvalussii 55 55 33 33 ja dáhtot ahte davvisámegielat bagadalli riŋge dutnje. Muite addit riegadan- ja persunnummara ja maid telefunnummara masa davvisámegielat galga riŋget.'}
+                    {
+                        'Diŋgo dás davvisámegilli bálvalusa mas vástiduvvo dutnje sámegillii buot NAV – bálvalusain ja oajuin. Mii veahkehit gávdnat mo du áššiin manná, ja veahkehit du dovdat rivttiid ja geatnegasvuođaid mat leat álbmotoadjolága njuolggadusain. Don gávnnat dieđuid iežat áššis neahttabálvalusas nav.no Ditt NAV. Don sáhtát iskat mii dutnje lea máksojuvvon dás:'
+                    }
+                    <br />
+                    <br />
+                    {
+                        'Don sáhtat ain riŋget NAV-bálvalussii 55 55 33 33 ja dáhtot ahte davvisámegielat bagadalli riŋge dutnje. Muite addit riegadan- ja persunnummara ja maid telefunnummara masa davvisámegielat galga riŋget.'
+                    }
                 </BodyLong>
             </GuidePanel>
+            <SamiskSamtaleOrderForm />
         </div>
     );
-}
+};
--- src/components/form/samisk-samtale-order-form/SamiskSamtaleOrderForm.tsx
+++ src/components/form/samisk-samtale-order-form/SamiskSamtaleOrderForm.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import style from './SamiskSamtaleOrderForm.module.css';
+import {
+    Button,
+    Checkbox,
+    CheckboxGroup,
+    Fieldset,
+    Panel,
+    TextField,
+} from '@navikt/ds-react';
 
 export const SamiskSamtaleOrderForm = () => {
-    return <div className={style.form}>{'Form goes here'}</div>
-}
+    return (
+        <Panel className={style.panel}>
+            <Fieldset legend={''}>
+                <TextField label="Ovdanamma" />
+                <TextField label="Goargu" />
+                <TextField label="Telefovdna*" />
+            </Fieldset>
+            <CheckboxGroup
+                legend="Goas heive duinna váldit oktavuođa?"
+                className={style.boxgroup}
+            >
+                <Checkbox value="morning">{'08.00-10.00'}</Checkbox>
+                <Checkbox value="afternoon">{'13.30-15.30'}</Checkbox>
+            </CheckboxGroup>
+            <Button className={style.button}>{'Sádde jearaldaga'}</Button>
+        </Panel>
+    );
+};
