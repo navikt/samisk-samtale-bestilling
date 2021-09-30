0c1509e849e6f70a5719a2ae468335d62224c7e6
anders-nom <anders@nomerstad.com>
2021-09-30 16:07:07 +0200

    Client-side api-url

--- next.config.js
+++ next.config.js
@@ -5,11 +5,12 @@ const withTranspileModules = require('next-transpile-modules')([
 
 module.exports = withTranspileModules({
     reactStrictMode: true,
-    ...(process.env.APP_BASEPATH && { basePath: process.env.APP_BASEPATH }),
+    basePath: process.env.APP_BASEPATH,
     env: {
         APP_ORIGIN: process.env.APP_ORIGIN,
         APP_BASEPATH: process.env.APP_BASEPATH,
         API_URL: process.env.API_URL,
+        KONTAKTINFO_API_URL: process.env.KONTAKTINFO_API_URL,
     },
     i18n: {
         locales: ['se'],
