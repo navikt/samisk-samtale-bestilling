7d9e8873615469495d1f586e1af42a0952f06ffe
Eivind Skifjeld <eivind.skifjeld@gmail.com>
2023-02-13 09:29:24 +0100

    La til &nbsp fordi understrekingen på nav.no var for lang

--- src/localization/nb.tsx
+++ src/localization/nb.tsx
@@ -28,9 +28,9 @@ export const localeModuleNb: {
             Her kan du bestille en telefonsamtale med NAV på nordsamisk. Vi
             hjelper deg med status i saken din og veileder deg om rettigheter og
             plikter. <br /> For å finne informasjon om dine saker og
-            utbetalinger, kan du logge inn på
-            <a href="https://www.nav.no/minside"> nav.no </a>. <br /> Du kan
-            også ringe NAV på <a href="tel:+4755553333">55 55 33 33</a>
+            utbetalinger, kan du logge inn på&nbsp;
+            <a href="https://www.nav.no/minside">nav.no</a>. <br /> Du kan også
+            ringe NAV på <a href="tel:+4755553333">55 55 33 33</a>
             &nbsp; og be om å bli kontaktet av en samisk veileder.
         </>
     ),
