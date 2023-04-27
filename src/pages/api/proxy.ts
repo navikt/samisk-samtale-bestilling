d16df2ea94f1ecdd46d74ac8c426003e528d27e8
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-04-27 17:25:42 +0200

    Går direkte mot tilbakemeldingsmottak i GCP

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,16 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import process from 'process';
+import { getAzureadToken } from '../../auth/azuread';
 
 const proxyHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const response = await fetch(`${process.env.API_URL}`, {
-            method: 'POST',
-            body: JSON.stringify(req.body),
-            headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-        });
+        const accessToken = await getAzureadToken(
+            `api://${process.env.ENV}-gcp.teamserviceklage.tilbakemeldingsmottak-api/.default`
+        );
+        const response = await fetch(
+            `${process.env.API_URL}/rest/mottak/bestilling-av-samtale`,
+            {
+                method: 'POST',
+                body: JSON.stringify(req.body),
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
         return res.status(response.status).json(response.body);
     } else {
-        return res.status(405).json("Method Not Allowed")
+        return res.status(405).json('Method Not Allowed');
     }
 };
 
