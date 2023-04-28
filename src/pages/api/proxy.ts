e6c2b833caf39f5d8e8ee36df9da7a5bea1d876c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-04-28 09:20:37 +0200

    Revert "Logging"
    This reverts commit 94de7aa1be9745237ce63642bb4753d0079d23da.

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -4,12 +4,9 @@ import { getAzureadToken } from '../../auth/azuread';
 
 const proxyHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        console.log("Henter token")
         const accessToken = await getAzureadToken(
             `api://${process.env.ENV}-gcp.teamserviceklage.tilbakemeldingsmottak-api/.default`
         );
-        console.log("Token hentet")
-        console.log(`Sender request til ${process.env.API_URL}/rest/mottak/bestilling-av-samtale`)
         const response = await fetch(
             `${process.env.API_URL}/rest/mottak/bestilling-av-samtale`,
             {
@@ -21,7 +18,6 @@ const proxyHandler = async (req: NextApiRequest, res: NextApiResponse) => {
                 },
             }
         );
-        console.log(`Mottatt respons med status ${response.status}`)
         return res.status(response.status).json(response.body);
     } else {
         return res.status(405).json('Method Not Allowed');
