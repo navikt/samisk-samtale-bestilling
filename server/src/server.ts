6534747d7e5af0d66fdd4f0f19f85cf4a13697af
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-10-16 10:52:27 +0200

    Fikser dotenv config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: `${__dirname}/../../.env` });
+dotenv.config({ path: '../.env' });
 
 import express from 'express';
 import compression from 'compression';
