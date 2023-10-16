e7c88af03b2b008bfa9441fa08f16ac039d33f01
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-10-16 12:36:35 +0200

    Fjerner logging av ting som ikke bør logges. Legger til react typer

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig(({ mode }) => {
     process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
     return {
-        plugins: [preact(), ...(process.env.ANALYZE ? [visualizer({ gzipSize: true, open: true, sourcemap: true })] : [])],
+        plugins: [preact(), ...(process.env.ANALYZE ? [visualizer({
+            gzipSize: true,
+            open: true,
+            sourcemap: true,
+        })] : [])],
         esbuild: {
             legalComments: 'none',
         },
@@ -17,7 +21,14 @@ export default defineConfig(({ mode }) => {
             // Dependencies containing React components must not be externalized
             // from the SSR bundle, in order to work with preact/compat. This
             // list must also include transitive dependencies.
-            noExternal: ['@navikt/ds-react', '@navikt/ds-icons', '@navikt/aksel-icons', '@navikt/ds-css', 'react-router', 'react-router-dom'],
+            noExternal: [
+                '@navikt/ds-react',
+                '@navikt/ds-icons',
+                '@navikt/aksel-icons',
+                '@navikt/ds-css',
+                'react-router',
+                'react-router-dom',
+            ],
         },
         base: process.env.VITE_APP_BASEPATH,
         css: {
