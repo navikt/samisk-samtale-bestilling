c86b3916eed9f8d5a8c2681fa36c6a5f9357c5b5
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-11-06 16:45:19 +0100

    Chagnes for decorator-next

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -1,7 +1,7 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            ENV: 'prod' | 'dev' | 'localhost';
+            ENV: 'prod' | 'dev' | 'dev-next' | 'localhost';
             VITE_APP_BASEPATH: string;
             APP_PORT: string;
             NODE_ENV: 'development' | 'production';
