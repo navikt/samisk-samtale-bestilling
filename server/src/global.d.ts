d6961c4bb07a0aa6cae2b1a29ff4c8de918fab73
Anders Nomerstad <anders@nomerstad.com>
2024-06-18 14:32:35 +0200

    decorator devNext -> dev

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -2,7 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             ENV: 'prod' | 'dev' | 'localhost';
-            DECORATOR_ENV: 'prod' | 'dev' | 'beta' | 'betaTms' | 'devNext' | 'prodNext' | 'localhost';
+            DECORATOR_ENV: 'prod' | 'dev' | 'beta' | 'betaTms' | 'localhost' | 'prodNext';
             VITE_APP_BASEPATH: string;
             APP_PORT: string;
             NODE_ENV: 'development' | 'production';
